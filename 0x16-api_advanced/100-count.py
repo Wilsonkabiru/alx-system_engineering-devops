e72abb9e9b8b29ea3650a198cf313be3f0822fe4
Wilsonkabiru <wkabiru19@gmail.com>
2023-05-09 02:49:47 -0700

    api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,48 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
 import requests
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def count_words(subreddit, word_list, after=None, counts=None):
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+    Counts the number of occurrences of each word in the given subreddit and prints the results in descending order.
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
+    :param subreddit: the name of the subreddit to search in (e.g. "python")
+    :param word_list: a list of words to count the occurrences of
+    :param after: the "after" parameter for pagination (optional, used for recursive calls)
+    :param counts: a dictionary containing the counts for each word (optional, used for recursive calls)
+    :return: None
+    """
+    if counts is None:
+        counts = {}
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "Mozilla/5.0"}  # Reddit API requires a User-Agent header
+
+    # Set the "after" parameter if it's provided
+    params = {}
+    if after:
+        params["after"] = after
+
+    # Send the request and handle the response
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        return  # Invalid subreddit, do nothing
+    data = response.json()
+
+    # Count the occurrences of each word in the titles
+    for post in data["data"]["children"]:
+        title = post["data"]["title"].lower()
         for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+            if word.lower() in title and not title.endswith(word.lower() + ".") and not title.endswith(word.lower() + "!") and not title.endswith(word.lower() + "_"):
+                counts[word] = counts.get(word, 0) + title.count(word.lower())
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
+    # Check if there are more pages to fetch
+    if data["data"]["after"] is not None:
+        # Recursively call the function with the "after" parameter
+        count_words(subreddit, word_list, after=data["data"]["after"], counts=counts)
     else:
-        count_words(subreddit, word_list, instances, after, count)
+        # Print the counts in descending order
+        for word, count in sorted(counts.items(), key=lambda item: (-item[1], item[0])):
+            print(word.lower(), count)
+count_words("python", ["python", "django", "flask", "sqlalchemy", "requests"])
+
