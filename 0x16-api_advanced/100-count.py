f7105a6929e623c863d46e861a90d09b26651e79
Wilsonkabiru <wkabiru19@gmail.com>
2023-05-09 02:53:35 -0700

    api

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python3
-import requests
+"""Module for task 3"""
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    """
-    Counts the number of occurrences of each word in the given subreddit and prints the results in descending order.
+def count_words(subreddit, word_list, word_count={}, after=None):
+    """Queries the Reddit API and returns the count of words in
+    word_list in the titles of all the hot posts
+    of the subreddit"""
+    import requests
 
-    :param subreddit: the name of the subreddit to search in (e.g. "python")
-    :param word_list: a list of words to count the occurrences of
-    :param after: the "after" parameter for pagination (optional, used for recursive calls)
-    :param counts: a dictionary containing the counts for each word (optional, used for recursive calls)
-    :return: None
-    """
-    if counts is None:
-        counts = {}
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code != 200:
+        return None
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Mozilla/5.0"}  # Reddit API requires a User-Agent header
+    info = sub_info.json()
 
-    # Set the "after" parameter if it's provided
-    params = {}
-    if after:
-        params["after"] = after
+    hot_l = [child.get("data").get("title")
+             for child in info
+             .get("data")
+             .get("children")]
+    if not hot_l:
+        return None
 
-    # Send the request and handle the response
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code != 200:
-        return  # Invalid subreddit, do nothing
-    data = response.json()
+    word_list = list(dict.fromkeys(word_list))
 
-    # Count the occurrences of each word in the titles
-    for post in data["data"]["children"]:
-        title = post["data"]["title"].lower()
+    if word_count == {}:
+        word_count = {word: 0 for word in word_list}
+
+    for title in hot_l:
+        split_words = title.split(' ')
         for word in word_list:
-            if word.lower() in title and not title.endswith(word.lower() + ".") and not title.endswith(word.lower() + "!") and not title.endswith(word.lower() + "_"):
-                counts[word] = counts.get(word, 0) + title.count(word.lower())
+            for s_word in split_words:
+                if s_word.lower() == word.lower():
+                    word_count[word] += 1
 
-    # Check if there are more pages to fetch
-    if data["data"]["after"] is not None:
-        # Recursively call the function with the "after" parameter
-        count_words(subreddit, word_list, after=data["data"]["after"], counts=counts)
+    if not info.get("data").get("after"):
+        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
+        sorted_counts = sorted(word_count.items(),
+                               key=lambda kv: kv[1], reverse=True)
+        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
     else:
-        # Print the counts in descending order
-        for word, count in sorted(counts.items(), key=lambda item: (-item[1], item[0])):
-            print(word.lower(), count)
-count_words("python", ["python", "django", "flask", "sqlalchemy", "requests"])
-
+        return count_words(subreddit, word_list, word_count,
+                           info.get("data").get("after"))
